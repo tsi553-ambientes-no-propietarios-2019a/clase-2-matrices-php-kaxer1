d41ca1bd1dcff6c8a97fe80604330a4cdf6c44d6
kaxer1 <kevin.curay@epn.edu.ec>
2019-04-16 01:12:24 -0500

    nuevo commit

--- index.php
+++ index.php
@@ -1,38 +1,7 @@
 <?php 
 /*
-    $nombre = "Juan";
-
-    echo "Hola $nombre<br>"; //escribe el nombre de la variable
-    // "" --> interpreta el contenido
-    echo 'Hola $nombre'; //escribe todo la cadena de caracteres
-
-    $arr = array(
-        'key1' => 'value1',
-        'key2' => 'value2',
-        'key3' => 'value3',
-        'key4' => array('4.1','4.2'),
-        'key5' => 5,
-    );
-
-    $arr1 = array(1,2,3,4);
-
-    echo '<br>';
-    echo '<pre>';
-    print_r($arr);
-    echo '</pre>';
-
-    echo '<br>';
-    echo '<pre>';
-    print_r($arr1);
-    echo '</pre>';
-
-    foreach ($arr as $key => $value){
-        echo $key . ' ' ;
-        print_r($value);
-        echo '<br>';
-    }
+    KEVIN CURAY
 */
-
     $arr2 = array(
         array('Nombre',
               'Apellido',
@@ -62,11 +31,3 @@
     }
     echo "</table>";
     
-/*
-    <table>
-<tr>
-  <td><strong>Curso</strong></td>
-  <td><strong>Horas</strong></td>
-  <td><strong>Horario</strong></td>
-</tr> 
-*/
